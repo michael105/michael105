09c1a094c4110025bf2fb97ca05857c5492f1e1a
misc147 <misc7@posteo.net>
2025-12-30 23:21:53 +0000

    fix those chars

--- saucer42.cp850.pl
+++ saucer42.cp850.pl
@@ -1,1 +1,37 @@
+#!/bin/perl
+use warnings;
+no Error;       
+#use brain
+#use diagnostics;
 
+
+
+
+
+
+
+
+           sub'saucer{;-0;my $brain;not(($n=ord($c))&0600)and 
+               $_[2].=$c or saucer($_,substr($_,($n^0200)
+             <<  1,2),$_[2])while($c=substr($_[1],$brain  ++
+                       ,1));$_[2];} eval saucer 
+                             $_,/\xff(.*)/s 
+                              for join "", 
+                                 <DATA> 
+                                  
+
+
+
+
+                                __DATA__
+
+
+   ƒe ontiƒƒˆƒTo‚thertoci 
+En‰„nestw areƒgonobo.€whlianƒ„orld…”s.fr’†‡“•nu‚Š †e‚has Thprinisofd 
+‰g‚ ŒŠ‹esvi…bfsi—‚f…Œ…voy eˆ‰™ªš¡ly†,¹zg…ag‰t °a›ref';§w exp ¡e­
+„‚®ˆ˜e.€€l …–'€¿ÀIt²¶¢–shg†«¬ºÑa‡s‚ÁËSp£¥ÔÜ†s¢Ğ×â›´ãäip“wÃlƒ© ¤¦c¦ÈÉtÒ
+ÏØm±¯±ÿ¯°Íß
+ƒ‚ëace,
+‚Œ—¾ÎöøıÙ¸Şàêğòóˆ‘ûÿÛáéù¨°µş¼ÇÌÂÕİïÊÓÖì÷seek out¥§»e³ôõúèîñ·ÅÆÚ
+Ÿœ½Äæ­åçíü
+#__________<_+++___Beware_-_of_-_Agent_-_Smith\''s's___+++_>____________
