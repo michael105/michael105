8b55adcc3b58c0dcdd388c0674c3841f2df01a2b
misc147 <misc7@posteo.net>
2025-12-31 02:13:26 +0000

    notes

--- saucer42.cp437.pl
+++ saucer42.cp437.pl
@@ -38,3 +38,35 @@ En‰„nestw areƒgonobo.€whlianƒ„orld…”s.fr’†‡“•nu‚Š †e‚has Thprinisofd
 ### __________<_+++___Beware_-_of_-_Agent_-_Smith\''s's___+++_>____________
 ### Happy new year!  
 ### in Germany we say: Have a good slide! ^
+
+
+
+
+# ////
+# About the perl block..
+# It's just a decompression, recursively.
+# and the concluding .. eval saucer ..
+# the decompression, funnily enough, isn't 
+# that bad. also the compression.
+# it's about 50 percent, sometime also more, 70 percent.
+# denpends on the input, obviously.
+# and it's only usable for ascii texts, 0x20-0x7e
+# On the other hand, the decompression thorughput might succeed
+# all other bottlenecks, so this isn't that useless.
+# just a simple byte pair encoding, anyways.
+# 
+# Coding means art to me.
+# Albite, I don't know what 'art' is.
+# Me, my living? this might be about "art", not 'art'.
+# I don't know it.
+# Quite often I feel more attracted by music, crating
+# music of my own.
+#
+# What's this all about?
+#
+# Is there a difference, is there a connection?
+#
+# 
+#
+#
+
